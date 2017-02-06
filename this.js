2c296321b8119def073b31c3e0a842bc69a66933
adamsaparudin <adamsaparudin@gmail.com>
2017-02-06 11:28:17 +0700

    SELESAI MUNGKIN YAH ?

--- this.js
+++ this.js
@@ -2,10 +2,24 @@
 
 // GLOBAL CONTEXT
 
-
-
+// Bisa kah kamu mendapatkan versi node yang terinstall menggunakan this ? BISA
+// let counter = {
+//   val : 0,
+//   increment: function() {
+//       this.val += 1
+//     }
+//   }
+// }
 
 // CLASS CONTEXT
 
 
 // RELEASE 1
+class contohClass {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+let kelasBaru = new contohClass("Test")
+console.log(kelasBaru.name)
