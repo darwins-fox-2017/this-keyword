ba9705801ea881f8e3acdecd7c6e68d8d1efd47f
Diky Arga & Hepiska Franatagola <pair+dikyarga.hepiska@hacktiv8.com>
2017-02-06 14:11:26 +0700

    realeased version 0

--- this.js
+++ this.js
@@ -1,8 +1,14 @@
 "use strict"
 
 // GLOBAL CONTEXT
+this menunjuk ke variabel global  dari tempat this ini di deklarasi sebagai
+contoh jika didalam browser this menunjuk pada window,
 
-
+Misalnya untuk mengakses this NodeJS itu sendiri di Repl
+Ex. memanggil versi NodeJS : node --> this.process.version
+dari situ kita bisa mengambil bahwa this digunakan untuk memanggil semua method didalam node,
+dan dengan menggunakan this.[object/method]
+kita bisa memanggil objek didalammnya
 
 
 // CLASS CONTEXT
